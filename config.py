50a95e8f3dc696a23b148c8bec3c31e7dc6ad7aa
P4RZIVAL286 <105545318+P4RZIVAL286@users.noreply.github.com>
2022-11-19 00:37:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Development(Config):
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgresql://postgres:rvmUIJq4su33mmXluYj1@containers-us-west-129.railway.app:7714/railway"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOI4Bu8WtSS77Xi_q2ldONIiEPCTQZj0AxeDIg0NKcqrPlirk9NGmdsFjghqp0k5fj_uLl-Oj9hZb4WzVlGkWbb4AJkUCYdIJT0sS_5Z3dX0c38SB3n7sX7_WZQVfFLEMAYGxPK0t2qxvrH6DYx3L91xejWKD_Ie19kuZmdMrapsoXMuzbXm7yeb3F8vLyOc_Cazj9krtFMgaq_snypZpz9u_eBuYhIZLGHVmMEgxJt2uDe3tqAweYYDLYDwg0HRbsTgy_q1Whd8FAIqQeA802L0TnfS7k-2WI3tMPM2thp3aGkgY99l7Ik-2E6tu3lFGCACDYIz6rf77rpn7bgEudJPJzGM="
+    STRING_SESSION = ""
     # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "5741914122:AAER-QZNEbu2HRArim0HEPuQWpkLFkM9XDA"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
